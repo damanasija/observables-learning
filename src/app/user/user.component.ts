cfaf9b5c59654a86af215dfbded18d1721de7144
Damanjeet Asija <daman.asija@gmail.com>
2018-07-22 22:20:58 +0530

    added subject from rxjs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   id: number;
+  mySubscription: Subscription;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private usersService: UsersService) { }
 
   ngOnInit() {
     this.route.params
@@ -18,6 +23,32 @@ export class UserComponent implements OnInit {
           this.id = +params['id'];
         }
       );
+    const myObservable = Observable.create(
+      (observer: Observer<string>) => {
+        setTimeout(() => {
+          observer.next("First package");
+        }, 1000);
+        setTimeout(() => {
+          observer.next("second package");
+        }, 2000);
+        setTimeout(() => {
+          observer.complete();
+        }, 4000);
+      }
+    );
+
+    this.mySubscription = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: Error)=> { console.log(error); },
+      () => { console.log('Completed'); }
+    )
   }
 
+  ngOnDestroy() {
+    this.mySubscription.unsubscribe();
+  }
+
+  onActivate() {
+    this.usersService.usersActivated.next(this.id);
+  }
 }
